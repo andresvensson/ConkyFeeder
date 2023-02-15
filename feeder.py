024d8d62b04cc10023c6b1e3fbcc4de7d5e200b1
andresvensson <andresvensson2@gmail.com>
2023-02-16 00:08:34 +0100

    Building structure

--- feeder.py
+++ feeder.py
@@ -6,19 +6,58 @@ import secret
 
 
 class Get_Data:
-    """Get values from my database."""
-    def __init__(self, db_name, column, table) -> None:
-        self.db_name = db_name
-        self.column = column
-        self.table = table
-        self.data = self.fetcher()
+    """Get values from my database.
+    Construct a nice print.
+     Parse into two files.
+    Conky app will run concatenate (cat) those to the files"""
+
+    def __init__(self, request: str) -> None:
+        self.data = {}
+        self.db_name = None
+        self.table = None
+        self.column = None
+        self.msg = None
+        self.weather()
+        # if request == "weather":
+        #    self.weather()
+
+    def weather(self):
+        d = {}
+        self.db_name = "website"
+        self.column = "temperature"
+        tn = "weather_"
+        self.table = tn + "datarum"
+        d['datarum'] = self.fetcher()[0]
+        self.table = tn + "sovrum"
+        d['sovrum'] = self.fetcher()[0]
+        self.table = tn + "kitchen"
+        d['kitchen'] = self.fetcher()[0]
+
+        # return msg
+        self.table = tn + "outside"
+        d['outside'] = self.fetcher()
+        self.column = "*"
+        d['all'] = self.fetcher()
+
+        d['tot_entires'] = d['all'][0]
+        d['ts'] = d['all'][1]
+        d['outside'] = d['all'][3]
+        d['wind_speed'] = d['all'][9]
+        d['wind_deg'] = d['all'][10]
+        d['cloud'] = d['all'][12]
+        d['sunrise'] = d['all'][13]
+        d['sunset'] = d['all'][14]
+        d['status'] = d['all'][15]
+        for x in d:
+            print(x, ":", d[x])
+        #print(d)
+        pass
 
     def fetcher(self):
         h, u, p = secret.sql()
         db = pymysql.connect(host=h, user=u, passwd=p, db=self.db_name)
         c = db.cursor()
         sql_data = None
-        #sql_query = get_tablestring(table, column)
 
         try:
             c.execute(self.sql_query())
@@ -28,6 +67,7 @@ class Get_Data:
         except pymysql.Error as e:
             print("Error reading DB: %d: %s" % (e.args[0], e.args[1]))
             print("nothingness")
+            print("!!", self.db_name)
 
         if sql_data:
             return sql_data
@@ -47,33 +87,15 @@ class Get_Data:
             return "SELECT {} FROM {} ORDER BY value_id DESC LIMIT 1".format(self.column, self.table)
 
     def print_data(self):
-        return self.data[0]
+        return self.data
 
 
-class Write_Data(Get_Data):
-    """Callable presets that write a file for Conky my app"""
-    def __init__(self, db_name, column, table):
-        # super inherit variables from Get_Data
-        super().__init__(db_name, column, table)
-        print("subclass")
-
-    def weather(self):
-        database = "website"
-        column = "temperature"
-        table = ""
-        sql_query = "SELECT {} FROM {} ORDER BY value_id DESC LIMIT 1".format(column, table)
-
-    def construct(self):
-        write_dis = "Test"
-        self.table = "some table name"
-        return write_dis
-
-    def writefile(self):
-        with open("conky_assets/" + self.table + ".txt", "w") as f:
-            f.write(self.data)
+def writefile(self):
+    with open("conky_assets/" + self.table + ".txt", "w") as f:
+        f.write(str(self.msg))
 
 
-#class Rates(Get_Data):
+# class Rates(Get_Data):
 #    def __init__(self):
 #        super().__init__(self, instructions, database, column, table)
 #        print("subclass")
@@ -84,22 +106,14 @@ class Write_Data(Get_Data):
 #        pass
 
 
-def writefile(self, data):
-    with open(secret.file_path() + self.filename + ".txt", "w") as f:
-        f.write(self.msg)
-
-
 def start():
-    # make two files
     print("call Weather")
-    print("call Rates")
-    # test it:
-    # database, column, table
-    w_data = Get_Data("website", "temperature", "weather_datarum")
-    print(w_data.print_data())
-
-    # weather_msg = class.weather()
-    # write_file(weather_msg)
+    #init = Get_Data("weather")
+    Get_Data("weather")
+    # TODO
+    # Just skip the weather method. Collect all data in one go
+    # Parse and render two files
+    # Rename class and methods
 
 
 if __name__ == "__main__":
